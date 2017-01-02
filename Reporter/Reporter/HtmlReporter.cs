f44f24a9c86ecae55112c4d989b79a9ead934e74
VladimirVasilev <vladimir.vasilev@yahoo.com>
2017-01-02 14:44:08 +0200

    Test Automation Reporter

---
+++ Reporter/Reporter/HtmlReporter.cs
@@ -0,0 +1,152 @@
+﻿namespace Reporter
+{
+    using System.IO;
+    using System.Web.UI;
+    
+    public class HtmlReporter
+    {
+        private const string HtmlDocType = "<!DOCTYPE html>";
+        private const string CssRelType = "stylesheet";
+        private const string MainContainerId = "wrapper";
+        private const string HeaderContainerId = "header";
+        private const string TestCaseContainerClass = "test-case-container";
+        private const string TestStepsContainerClass = "test-steps-container";
+        private const string TestStepsSuccessClass = "success";
+        private const string TestStepsFailureClass = "failure";
+
+        public HtmlReporter()
+        {
+            this.StringWriter = new StringWriter();
+            this.Writer = new HtmlTextWriter(this.StringWriter);
+        }
+
+        public StringWriter StringWriter { get; set; }
+
+        public HtmlTextWriter Writer { get; set; }      
+
+        public void InitiateHtmlReport(string pageTitle, string cssPath, string headerText)
+        {
+            this.InitiateHtmlDoctype();
+            this.InitiateHead(pageTitle, cssPath);
+            this.InitiateBodyAndMainContainer();
+            this.InitiateHeader(headerText);
+        }
+
+        public void CompleteHtmlReportAndDispose()
+        {
+            this.Writer.RenderEndTag();
+            this.Writer.RenderEndTag();
+            this.Writer.RenderEndTag();
+            this.Writer.Dispose();
+            this.StringWriter.Dispose();
+        }
+
+        public void StartTest(string testDescription, string methodName)
+        {
+            this.Writer.AddAttribute(HtmlTextWriterAttribute.Class, TestCaseContainerClass);
+            this.Writer.RenderBeginTag(HtmlTextWriterTag.Div);
+
+            this.Writer.RenderBeginTag(HtmlTextWriterTag.H1);
+            this.Writer.Write(testDescription);
+            this.Writer.RenderEndTag();
+
+            this.Writer.RenderBeginTag(HtmlTextWriterTag.H2);
+            this.Writer.Write(methodName);
+            this.Writer.RenderEndTag();
+
+            this.Writer.AddAttribute(HtmlTextWriterAttribute.Class, TestStepsContainerClass);
+            this.Writer.RenderBeginTag(HtmlTextWriterTag.Ol);
+        }
+
+        public void EndTest()
+        {
+            this.Writer.RenderEndTag();
+            this.Writer.RenderBeginTag(HtmlTextWriterTag.Hr);
+            this.Writer.RenderEndTag();
+            this.Writer.RenderEndTag();
+        }
+
+        public void StartStep(string stepDescription)
+        {
+            this.Writer.RenderBeginTag(HtmlTextWriterTag.Li);
+            this.Writer.Write(stepDescription);            
+        }
+
+        public void EndStep(bool condition)
+        {
+            if (condition)
+            {
+                this.Writer.AddAttribute(HtmlTextWriterAttribute.Class, TestStepsSuccessClass);
+                this.Writer.RenderBeginTag(HtmlTextWriterTag.Span);
+                this.Writer.Write(" - Success!");
+                this.Writer.RenderEndTag();
+            }
+            else
+            {
+                this.Writer.AddAttribute(HtmlTextWriterAttribute.Class, TestStepsFailureClass);
+                this.Writer.RenderBeginTag(HtmlTextWriterTag.Span);
+                this.Writer.Write(" - Failure!");
+                this.Writer.RenderEndTag();
+            }
+
+            this.Writer.RenderEndTag();
+        }
+
+        public void EndStep()
+        {
+            this.EndStep(true);
+        }
+        
+        private void InitiateHtmlDoctype()
+        {
+            this.Writer.Write(HtmlDocType);
+            this.Writer.RenderBeginTag(HtmlTextWriterTag.Html);
+        }
+
+        private void InitiateHead(string pageTitle, string cssFilePath)
+        {
+            this.Writer.RenderBeginTag(HtmlTextWriterTag.Head);
+            this.Writer.RenderBeginTag(HtmlTextWriterTag.Title);
+
+            if (!string.IsNullOrEmpty(pageTitle))
+            {                
+                this.Writer.Write(pageTitle);
+            }
+
+            this.Writer.RenderEndTag();
+
+            if (!string.IsNullOrEmpty(cssFilePath))
+            {
+                this.Writer.AddAttribute(HtmlTextWriterAttribute.Href, cssFilePath);
+                this.Writer.AddAttribute(HtmlTextWriterAttribute.Rel, CssRelType);                
+            }
+
+            this.Writer.RenderBeginTag(HtmlTextWriterTag.Link);
+            this.Writer.RenderEndTag();
+            this.Writer.RenderEndTag();
+        }
+
+        private void InitiateBodyAndMainContainer()
+        {
+            this.Writer.RenderBeginTag(HtmlTextWriterTag.Body);
+
+            this.Writer.AddAttribute(HtmlTextWriterAttribute.Id, MainContainerId);
+            this.Writer.RenderBeginTag(HtmlTextWriterTag.Div);
+        }
+
+        private void InitiateHeader(string headerText)
+        {
+            this.Writer.AddAttribute(HtmlTextWriterAttribute.Id, HeaderContainerId);
+            this.Writer.RenderBeginTag(HtmlTextWriterTag.Div);
+
+            this.Writer.RenderBeginTag(HtmlTextWriterTag.H1);
+            if (!string.IsNullOrEmpty(headerText))
+            {                
+                this.Writer.Write(headerText);                
+            }
+
+            this.Writer.RenderEndTag();
+            this.Writer.RenderEndTag();
+        }
+    }
+}
---
+++ Reporter/Reporter/ReporterTest.cs
@@ -0,0 +1,44 @@
+﻿namespace Reporter
+{
+    public class ReporterTest
+    {
+        public static void Main(string[] args)
+        {
+            HtmlReporter reporter = new HtmlReporter();
+
+            int testCasesCount = 7;
+            int testStepsCount = 12;
+
+            string pageTitle = "Product - Test Report ";
+            string cssFilePath = "styles/style.css";
+            string headerText = "Product Name - Test Automation Results";
+
+            reporter.InitiateHtmlReport(pageTitle, cssFilePath, headerText);
+
+            for (int i = 1; i <= testCasesCount; i++)
+            {
+                reporter.StartTest("Test case " + i + " description", "testMethod" + i);
+
+                for (int j = 1; j <= testStepsCount; j++)
+                {
+                    reporter.StartStep("Test step description " + j);
+
+                    if (j % 2 == 0)
+                    {
+                        reporter.EndStep();
+                    }
+                    else
+                    {
+                        reporter.EndStep(false);
+                    }
+                }
+
+                reporter.EndTest();
+            }
+
+            reporter.CompleteHtmlReportAndDispose();
+
+            System.IO.File.WriteAllText("../../test.html", reporter.StringWriter.ToString());
+        }
+    }
+}
